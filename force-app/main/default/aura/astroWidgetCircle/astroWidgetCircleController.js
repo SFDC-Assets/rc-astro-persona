00a1d8988aa5bb9f1a9ac7db4d5e1c33b862026a
Ryan Cox <congmingwudi@gmail.com>
2020-10-27 01:24:29 -0600

    removed need for zip of images static resource

--- force-app/main/default/aura/astroWidgetCircle/astroWidgetCircleController.js
+++ force-app/main/default/aura/astroWidgetCircle/astroWidgetCircleController.js
@@ -2,6 +2,9 @@
     doInit: function(cmp, evt, helper) {      
     	var astroImage = cmp.get("v.astroImage");
     	console.log('astroWidgetCircleController - astroImage: ' + astroImage);
+        var astroImageResource = $A.get('$Resource.' + astroImage);
+        cmp.set("v.astroImageResource", astroImageResource);
+        console.log('astroWidgetCircleController - astroImageResource: ' + astroImageResource);
     },
     
     handleToggleEvent: function(cmp, evt, helper) {
--- force-app/main/default/aura/astroWidgetIndex/astroWidgetIndexController.js
+++ force-app/main/default/aura/astroWidgetIndex/astroWidgetIndexController.js
@@ -2,5 +2,7 @@
 	doInit: function(cmp, evt, helper) {      
     	var persona = cmp.get("v.persona");
     	console.log('astroWidgetIndexController - persona: ' + JSON.stringify(persona));
+        var widgetHeight = cmp.get("v.widgetHeight");
+    	console.log('astroWidgetIndexController - widgetHeight: ' + widgetHeight);
     },
 })
